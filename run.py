75c371e447470cebddd774b487fbdc7d5c61d463
juliandavid-garcia <juliandavid.garcia@gmail.com>
2021-09-15 23:42:45 +0000

    added member.html and and linked the url to about

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ def index():
 @app.route("/about")
 def about():
     data = []
-    with open ("data/company.json","r") as json_data:
+    with open("data/company.json","r") as json_data:
         data = json.load(json_data)
 
     return render_template("about.html", page_title ="About", company=data)
@@ -24,10 +24,10 @@ def about():
 @app.route("/about/<member_name>")
 def about_member(member_name):
     member = {}
-    with open ("data/company.json","r") as json_data:
+    with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
-            if obj[url] == member_name:
+            if obj["url"] == member_name:
                 member = obj
     return render_template("member.html", member=member)
 
